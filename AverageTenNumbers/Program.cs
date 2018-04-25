ee25b94811271e9c0b6fc43a61c8b0bc4552b6ba
nikesh-maharjan <nikesh.cap@gmail.com>
2018-04-25 10:42:11 -0400

    initial commit

---
+++ AverageTenNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using C_Sharp_Module2_Exercise2A_SumOfNumbers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Module2_Exercise2A_SumOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] inputs = new double[10];
+            double sum = 0;
+            for (int i=0; i<10; i++)
+            {
+                do
+                {
+                    inputs[i] = UserInput.UserInputScore();
+                    if (inputs[i] < 0 || inputs[i] > 100)
+                        Console.WriteLine("Invalid Input. Re-Enter data");
+                } while (inputs[i] < 0 || inputs[i] > 100);
+            sum = sum + inputs[i];
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Sum of the scores is {0}", UserInput.sumArray(inputs));
+        }
+    }
+}
+
+
---
+++ AverageTenNumbers/UserInputs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Module2_Exercise2A_SumOfNumbers
+{
+    class UserInput
+    {
+        public static double UserInputScore()
+        {
+            Console.Write("Enter a number between 0 to 100: ");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        public static double sumArray(double[] array)
+        {
+            double sum = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                sum = sum + array[i];
+            }
+            return sum;
+        }
+    }
+
+}
